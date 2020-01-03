8c74999c6f4aa8ab32c347164190301dfcff7bbd
Divine Touch <divine.touch07@gmail.com>
2020-01-02 22:31:08 -0600

    update categories page

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,27 +6,22 @@ class CategoriesScreen extends StatelessWidget {
   static const id = '/categories';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeal'),
-      ),
-      body: GridView(
-        padding: EdgeInsets.all(15.0),
-        children: DUMMY_CATEGORIES
-            .map(
-              (categoryData) => CategoryItem(
-                id: categoryData.id,
-                title: categoryData.title,
-                color: categoryData.color,
-              ),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200.0,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20.0,
-            mainAxisSpacing: 20.0),
-      ),
+    return GridView(
+      padding: EdgeInsets.all(15.0),
+      children: DUMMY_CATEGORIES
+          .map(
+            (categoryData) => CategoryItem(
+              id: categoryData.id,
+              title: categoryData.title,
+              color: categoryData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20.0,
+          mainAxisSpacing: 20.0),
     );
   }
 }
